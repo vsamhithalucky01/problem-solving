3afc98a1cb293e309bccb8b1ba4ea0a02b39386e
vsamhithalucky01 <vsamhithalucky@gmail.com>
2026-08-28 09:04:48 +0530

    Update session1.java

--- week5/session1.java
+++ week5/session1.java
@@ -1,25 +1,13 @@
-import java.util.Scanner;
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int currentSum = nums[0];
+        int maxSum = nums[0];
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter attendance percentage: ");
-        double attendance = sc.nextDouble();
-
-        System.out.print("Enter marks: ");
-        int marks = sc.nextInt();
-
-        System.out.print("Enter number of backlogs: ");
-        int backlogs = sc.nextInt();
-
-        // Constraints
-        if (attendance >= 75 && marks >= 50 && backlogs == 0) {
-            System.out.println("Student is Eligible");
-        } else {
-            System.out.println("Student is Not Eligible");
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
         }
 
-        sc.close();
+        return maxSum;
     }
 }
