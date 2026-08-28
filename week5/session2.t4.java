61bf56edd7e64e4b644f4975b9b53cd213b7f7b3
vsamhithalucky01 <vsamhithalucky@gmail.com>
2026-08-28 09:34:02 +0530

    Add files via upload

---
+++ week5/session2.t4.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int maxSubarraySumCircular(int[] nums) {
+        int total = 0;
+
+        int maxSum = nums[0];
+        int curMax = 0;
+
+        int minSum = nums[0];
+        int curMin = 0;
+
+        for (int num : nums) {
+            curMax = Math.max(num, curMax + num);
+            maxSum = Math.max(maxSum, curMax);
+
+            curMin = Math.min(num, curMin + num);
+            minSum = Math.min(minSum, curMin);
+
+            total += num;
+        }
+
+        // If all elements are negative
+        if (maxSum < 0) {
+            return maxSum;
+        }
+
+        return Math.max(maxSum, total - minSum);
+    }
+}
