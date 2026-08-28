30aeff9d2bd2567d0d06f709ba738edd4c526a44
vsamhithalucky01 <vsamhithalucky@gmail.com>
2026-08-28 09:39:37 +0530

    Add files via upload

---
+++ week5/session3.t5.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int myAtoi(String s) {
+        int i = 0;
+        int n = s.length();
+
+        // 1. Skip spaces
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        // 2. Check sign
+        int sign = 1;
+        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+            if (s.charAt(i) == '-') {
+                sign = -1;
+            }
+            i++;
+        }
+
+        // 3. Convert digits
+        long num = 0;
+
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            num = num * 10 + (s.charAt(i) - '0');
+
+            // 4. Check overflow
+            if (sign == 1 && num > Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
+            }
+
+            if (sign == -1 && -num < Integer.MIN_VALUE) {
+                return Integer.MIN_VALUE;
+            }
+
+            i++;
+        }
+
+        return (int) (sign * num);
+    }
+}
